19261b145c2c05ba8bb34e67fc6d86a38a51f00e
zumrat <zuma.shukurova@gmail.com>
2021-11-23 01:07:57 +0200

    php assigment

---
+++ output.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <title>Output</title>
+</head>
+<h1>
+Form Output
+</h1>
+<body>
+    <div class="container">
+<p><?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?> date of birth is:
+<br>
+<br>
+ <?php echo $_POST["dob"]; ?></p>
+    </div>
+</body>
+</html>
